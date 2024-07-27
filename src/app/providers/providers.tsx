7662d94684c2ec5c17231e5f775e6ad917dcaa89
excel <danil_gorokhov@list.ru>
2024-07-27 18:47:25 +0500

    feat: add error boundary provider

--- src/app/providers/providers.tsx
+++ src/app/providers/providers.tsx
@@ -1,11 +1,17 @@
 import { store } from 'app/store'
 import { FC } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
 import { Provider } from 'react-redux'
+import { Fallback } from 'shared/ui/fallback'
 
 interface IProviders {
 	readonly children: JSX.Element
 }
 
 export const Providers: FC<IProviders> = ({ children }) => {
-	return <Provider store={store}>{children}</Provider>
+	return (
+		<ErrorBoundary FallbackComponent={Fallback}>
+			<Provider store={store}>{children}</Provider>
+		</ErrorBoundary>
+	)
 }
